c2e0f6858bd22d5b1f14a5b41b4baa9dc7a4b96e
kimyounghun1 <hunhoohunhoo1@gmail.com>
2019-11-11 21:11:33 +0900

    material design chip 사용

--- app/src/main/java/com/example/mediaproject/Activity/AcountActivity.java
+++ app/src/main/java/com/example/mediaproject/Activity/AcountActivity.java
@@ -1,9 +1,11 @@
 package com.example.mediaproject.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.mediaproject.R;
+import com.google.android.material.chip.Chip;
 
 public class AcountActivity extends BaseActivity {
 
@@ -14,8 +16,14 @@ public class AcountActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentLayout(R.layout.activity_account);
         getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-
+        Chip chip = (Chip) findViewById(R.id.chip);
+        chip.setLayoutDirection(View.LAYOUT_DIRECTION_LOCALE);
+        chip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click.
+            }
+        });
 
 
     }
